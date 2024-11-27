1a3e38e8bb77f6fea1e1445b5947bb186efdc3ed
ZorroGuardaPavos <4ndres.or@gmail.com>
2024-11-27 16:58:08 +0100

    Refactor test fixtures: move sample_tree fixture to trees/conftest.py and clean up unused imports in backend/tests/conftest.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,15 +4,9 @@ from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 from neo4j import GraphDatabase
-from neomodel import config
-from src.core.config import settings
 from src.main import app
 
-from tests.utils.utils import populate_database
-
 logger = logging.getLogger(__name__)
-
-config.DATABASE_URL = settings.DATABASE_URL
 
 
 @pytest.fixture(scope='session')
@@ -51,19 +45,3 @@ def cleanup(db_connection):
 def client() -> Generator[TestClient, None, None]:
     with TestClient(app) as c:
         yield c
-
-
-@pytest.fixture
-def sample_tree(db_session):
-    JSON_DATA = {
-        'data': [
-            {'name': 'A', 'description': 'This is a description of A', 'parent': ''},
-            {'name': 'B', 'description': 'This is a description of B', 'parent': 'A'},
-            {'name': 'C', 'description': 'This is a description of C', 'parent': 'A'},
-            {'name': 'D', 'description': 'This is a description of D', 'parent': 'A'},
-            {'name': 'B-1', 'description': 'This is a description of B-1', 'parent': 'B'},
-            {'name': 'B-2', 'description': 'This is a description of B-2', 'parent': 'B'},
-            {'name': 'B-3', 'description': 'This is a description of B-3', 'parent': 'B'},
-        ]
-    }
-    return populate_database(JSON_DATA)
---
+++ backend/tests/trees/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from tests.utils.utils import populate_database
+
+
+@pytest.fixture
+def sample_tree(db_session):
+    JSON_DATA = {
+        'data': [
+            {'name': 'A', 'description': 'This is a description of A', 'parent': ''},
+            {'name': 'B', 'description': 'This is a description of B', 'parent': 'A'},
+            {'name': 'C', 'description': 'This is a description of C', 'parent': 'A'},
+            {'name': 'D', 'description': 'This is a description of D', 'parent': 'A'},
+            {'name': 'B-1', 'description': 'This is a description of B-1', 'parent': 'B'},
+            {'name': 'B-2', 'description': 'This is a description of B-2', 'parent': 'B'},
+            {'name': 'B-3', 'description': 'This is a description of B-3', 'parent': 'B'},
+        ]
+    }
+    return populate_database(JSON_DATA)
